3ddcad7290b9f347d28b9052d37b95013049a105
gbm618 <gbm618@gmail.com>
2017-11-28 16:45:43 +0800

    df4 freertos project

---
+++ bat0.bat
@@ -0,0 +1,6 @@
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj
+cp pcubemx/MDK-ARM/pcubemx.uvopt.000 pcubemx/MDK-ARM/pcubemx.uvopt
+cp pcubemx/MDK-ARM/pcubemx.uvproj.000 pcubemx/MDK-ARM/pcubemx.uvproj
+ll pcubemx/MDK-ARM
+
---
+++ batpre.bat
@@ -0,0 +1,13 @@
+rm -f pcubemx/.mxproject
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt.000
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj.000
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt.001
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj.001
+cp pcubemx/MDK-ARM/pcubemx.uvopt pcubemx/MDK-ARM/pcubemx.uvopt.000
+cp pcubemx/MDK-ARM/pcubemx.uvproj pcubemx/MDK-ARM/pcubemx.uvproj.000
+cp pcubemx/MDK-ARM/pcubemx.uvopt pcubemx/MDK-ARM/pcubemx.uvopt.001
+cp pcubemx/MDK-ARM/pcubemx.uvproj pcubemx/MDK-ARM/pcubemx.uvproj.001
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj
+ll pcubemx/MDK-ARM
+
